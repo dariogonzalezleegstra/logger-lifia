23325d8bf4030d03e619d71695327999b8acc755
dariogonzalezleegstra <daroo.lp@gmail.com>
2019-03-12 10:13:11 -0300

    add keys

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:'57617933602-69rcgusuarphkoscf26ckqjneeg74c34.apps.googleusercontent.com',
+    googleClientSecret: 't3-nzQb7S8HQYhlJnKL3nbKx',
+    mongoURI: 'mongodb://lifia50:lifia2525@ds141815.mlab.com:41815/logger',
+    cookieKey: 'lfsdlkfmdlkgmdfhhkfgkmfd',
+    stripePublishableKey: 'pk_test_FFP8Ij5X903HJ0lNHsPMCz7H',
+    stripeSecretKey: 'sk_test_LMqJLQBRgkqLijNbPu2fXcUt'
+};
